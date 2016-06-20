f2e3738aaa782d8de85607275ae8d20847a3f120
Leif Dalan <leifdalan@gmail.com>
2016-06-20 11:50:56 -0700

    Added Procfile, mailgun stuff.

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const config = process.env.NODE_ENV === 'production'
+  ? process.env
+  : require('./local');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.post('/complete', function (req, res) {
+  // console.error('req', req);
+  var transporter
+    , message;
+
+  transporter = nodemailer.createTransport({
+    service: 'Mailgun',
+    auth: {
+      user: MAILGUN_USER,
+      pass: MAILGUN_PASS
+    }
+  });
+  message = {
+    from: 'YourServer ',
+    to: 'leifdalan@gmail.com', // comma separated list
+    subject: 'Subject Line',
+    text: 'Text contents.',
+    html: '<b>html contents.</b>'
+  };
+  transporter.sendMail(message, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      res.send('cool.')
+      console.log('Sent: ' + info.response);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
